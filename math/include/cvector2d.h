27edd56b91fab349191058d7ae092ee3ee22f8b0
Duadua <270389972@qq.com>
2019-02-26 11:42:47 +0800

    other test wifi

--- math/include/cvector2d.h
+++ math/include/cvector2d.h
@@ -4,16 +4,16 @@ class CVector3D;
 
 class CVector2D {
 public:
-	CVector2D(float a, float b) : _x(a), _y(b) {}
-	CVector2D(float a = 0.0f) : _x(a), _y(a) {}
+	CVector2D(float a, float b) { v[0] = a; v[1] = b; }
+	CVector2D(float a = 0.0f) { v[0] = v[1] = a; }
 	CVector2D(const CVector3D& abc);
 	~CVector2D() {}
 	
-	void set_x(float a) { _x = a; }
-	void set_y(float b) { _y = b; }
+	void set_x(float a) { v[0] = a; }
+	void set_y(float b) { v[1] = b; }
 	
-	float x() const { return _x; }
-	float y() const { return _y; }
+	float x() const { return v[0]; }
+	float y() const { return v[1]; }
 	
 	CVector2D operator + (const CVector2D& b) const;
 	CVector2D operator - (const CVector2D& b) const;
@@ -30,7 +30,6 @@ public:
 	float length() const;
 
 private:
-	float _x;
-	float _y;
+	float v[2];
 
 };
--- math/source/cvector2d.cpp
+++ math/source/cvector2d.cpp
@@ -2,21 +2,21 @@
 #include "cvector3d.h"
 #include <cmath>
 
-CVector2D::CVector2D(const CVector3D& abc) { _x = abc.x(); _y = abc.y(); }
+CVector2D::CVector2D(const CVector3D& abc) { v[0] = abc.x(); v[1] = abc.y(); }
 
-CVector2D CVector2D::operator + (const CVector2D& b) const { return CVector2D(_x + b.x(), _y + b.y()); }
-CVector2D CVector2D::operator - (const CVector2D& b) const { return CVector2D(_x - b.x(), _y - b.y()); }
-CVector2D CVector2D::operator * (const CVector2D& b) const { return CVector2D(_x * b.x(), _y * b.y()); }
-CVector2D CVector2D::operator * (const float& b) const { return CVector2D(_x * b, _y * b); }
+CVector2D CVector2D::operator + (const CVector2D& b) const { return CVector2D(v[0] + b.x(), v[1] + b.y()); }
+CVector2D CVector2D::operator - (const CVector2D& b) const { return CVector2D(v[0] - b.x(), v[1] - b.y()); }
+CVector2D CVector2D::operator * (const CVector2D& b) const { return CVector2D(v[0] * b.x(), v[1] * b.y()); }
+CVector2D CVector2D::operator * (const float& b) const { return CVector2D(v[0] * b, v[1] * b); }
 CVector2D CVector2D::operator / (const CVector2D& b) const {
 	if (b.x() == 0.0f || b.y() == 0.0f) return CVector2D();
-	return CVector2D(_x / b.x(), _y / b.y());
+	return CVector2D(v[0] / b.x(), v[1] / b.y());
 }
-CVector2D CVector2D::operator +=(const CVector2D& b) { _x += b.x(); _y += b.y(); return (*this); }
+CVector2D CVector2D::operator +=(const CVector2D& b) { v[0] += b.x(); v[1] += b.y(); return (*this); }
 
-float CVector2D::dot(const CVector2D& b) const { return _x * b.x() + _y * b.y(); }
-float CVector2D::cross(const CVector2D& b) const { return _x * b.y() - b.x()*_y; }
+float CVector2D::dot(const CVector2D& b) const { return v[0] * b.x() + v[1] * b.y(); }
+float CVector2D::cross(const CVector2D& b) const { return v[0] * b.y() - b.x() * v[1]; }
 
-CVector2D CVector2D::normalize() { float len = length(); _x /= len; _y /= len; return (*this); }
+CVector2D CVector2D::normalize() { float len = length(); v[0] /= len; v[1] /= len; return (*this); }
 
-float CVector2D::length() const { return std::sqrt(_x*_x + _y*_y); }
+float CVector2D::length() const { return std::sqrt(v[0]*v[0] + v[1]*v[1]); }
