4dc8be71bd512befba927b4b0b1bb633e6799b80
Duadua <270389972@qq.com>
2019-02-28 18:26:45 +0800

    update ready to complete quaternion

--- math/include/cquaternion.h
+++ math/include/cquaternion.h
@@ -7,13 +7,24 @@
 
 class CQuaternion {
 public:
-	CQuaternion(){}
+	CQuaternion(float a, float b, float c, float d) : x(a), y(b), z(c), w(d) { normalize(); }
+	CQuaternion(const CVector3D& euler_angle);
+	CQuaternion(const CVector4D& angle_axis);
 	~CQuaternion(){}
-	CQuaternion(const CVector3D& euler_angle){}
-	CQuaternion(const CVector4D& angle_axis){}
 
-	CVector3D get_euler_angle() const;
-	CVector4D get_angle_axis() const;				// (angle, x, y , z)
+	CVector3D get_euler_angle() const;				// (pitch, yaw, roll) -- (x, y, z)
+	CVector4D get_angle_axis() const;				// (angle, x, y, z)
+
+	static CQuaternion llerp(const CQuaternion& a, const CQuaternion& b);	// linear lerp
+	static CQuaternion slerp(const CQuaternion& a, const CQuaternion& b);	// spherical lerp
+
 private:
+
+	float x;			// x = v_x * sin(angle / 2)
+	float y;			// y = v_y * sin(angle / 2)
+	float z;			// z = v_z * sin(angle / 2)
+	float w;			// w = cos(angle / 2)
+
+	void normalize();	// x*x + y*y + z*z + w*w = 1
 
 };
--- math/source/cquaternion.cpp
+++ math/source/cquaternion.cpp
@@ -1,10 +1,27 @@
 #include "cquaternion.h"
 
+CQuaternion::CQuaternion(const CVector3D& euler_angle) {
+
+}
+CQuaternion::CQuaternion(const CVector4D& angle_axis) {
+
+}
 
 CVector3D CQuaternion::get_euler_angle() const {
 	return CVector3D();
 }
 CVector4D CQuaternion::get_angle_axis() const {
 	return CVector4D();
+
+}
+
+CQuaternion CQuaternion::llerp(const CQuaternion& a, const CQuaternion& b) {
+	return CQuaternion();
+}
+CQuaternion CQuaternion::slerp(const CQuaternion& a, const CQuaternion& b) {
+	return CQuaternion();
+}
+
+void CQuaternion::normalize() {
 
 }
